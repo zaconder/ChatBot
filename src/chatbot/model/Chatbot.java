6ad0b0119ebf0827720cb2b8ead887d4fedb4d76
zaconder <zac@conder.us>
2014-09-26 13:11:47 -0600

    Added more documentation

--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -1,8 +1,13 @@
 package chatbot.model;
 
+/**
+ * The chatbot model class. Used for checking and manipulationg Strings.
+ * @author zcon5199
+ * @version 1.1 9/26/14
+ */
 public class Chatbot
 {
-	
+
 	private String name;
 	private int chatCount;
 	
@@ -16,26 +21,52 @@ public class Chatbot
 		chatCount = 0;
 	}
 	
+	
+	/**
+	 * Returns the name of the Chatbot object.
+	 * @return The current name of the Chatbot.
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	
+	/**
+	 * It will return the number of times the Chatbot has responded.
+	 * @return The current number of chats
+	 */
 	public int getChatCount()
 	{
 		return chatCount;
 	}
 	
+	
+	/**
+	 * Creates a string that shows the name of the user.
+	 * @param name The supplied name for the user.
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 	
+	
+	/**
+	 * Updates and adds one to the chat count everytime that param is called 
+	 * @updateChatCount
+	 */
 	private void updateChatCount()
 	{
 		chatCount++;
 	}
 	
+	
+	/**
+	 * Checks the input for a certain variable and will quit if true
+	 * @param input If input is equal to sayonara than it will quit.
+	 * @return okToQuit will check the input and return it's value of true or false.
+	 */
 	public boolean quitChecker(String input)
 	{
 		boolean okToQuit = false;
