58612c17036c2236a38a11b8046c546c66a6cf89
zaconder <zac@conder.us>
2014-10-21 13:19:25 -0600

    Added documentation / Layout Options
    did the setupLayout setters

--- src/chatbot/view/ChatbotFrame.java
+++ src/chatbot/view/ChatbotFrame.java
@@ -4,10 +4,21 @@ import javax.swing.JFrame;
 
 import chatbot.controller.ChatbotAppController;
 
+/**
+ * GUI Frame class for a Chatbot. Shows the basic framework code for a JFrame extension
+ * @author zcon5199
+ * @version 1.1 10/21/14
+ */
 public class ChatbotFrame extends JFrame
 {
+	/**
+	 * Reference for the ChatbotPanel of the GUI.
+	 */
 	private ChatbotPanel basePanel;
-	
+	/**
+	 * Creates a ChatbotFrame with a reference to the ChatbotAppController to pass to the panel for MCV.
+	 * @param baseController The reference to the controller object.
+	 */
 	public ChatbotFrame(ChatbotAppController baseController) 
 	{
 		basePanel = new ChatbotPanel(baseController);
@@ -15,6 +26,9 @@ public class ChatbotFrame extends JFrame
 		setupFrame();
 	}
 
+	/**
+	 * Helper method to load panel into frame, set size, and visibility
+	 */
 	private void setupFrame()
 	{
 		this.setContentPane(basePanel);
--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,15 +1,68 @@
 package chatbot.view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
 
+
 public class ChatbotPanel extends JPanel
 {
+	/**
+	 * This will call the ChatbotAppController and names it "baseController"
+	 */
 	private ChatbotAppController baseController;
+	/**
+	 * This will create a button called "firstButton"
+	 */
+	private JButton firstButton;
+	/**
+	 * this will Create a TextField called "firstTextField"
+	 */
+	private JTextField firstTextField;
+	/**
+	 * This will call the SpringLayout method and names it "baseLayout"
+	 */
+	private SpringLayout baseLayout;
+	
 	public ChatbotPanel(ChatbotAppController baseController) 
 	{
 		this.baseController = baseController;
+		
+		firstButton = new JButton("click the button... it is so clicky :D");
+		firstTextField = new JTextField(25);
+		baseLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
 
+	private void setupPanel()
+	{
+		this.setBackground(Color.MAGENTA);
+		this.setSize(400, 400);
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 157, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 153, SpringLayout.WEST, firstTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, -39, SpringLayout.SOUTH, this);
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
 }
